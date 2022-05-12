13e8a16b896681b5a0c48eccb933098d566ad3bb
sharipoff-0-1 <tedcodes.01@gmail.com>
2022-05-12 18:26:24 +0200

    small change

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,4 +1,4 @@
-export default (posts = [], action) => {
+const reducer = (posts = [], action) => {
   switch (action.type) {
     case "FETCH_ALL":
       return action.payload;
@@ -15,3 +15,5 @@ export default (posts = [], action) => {
       return posts;
   }
 };
+
+export default reducer;
